3ffe854674b2800eea39b77b7a8d1faf9418521d
Felipe Macedo Gomes <104227174+femacedogomes@users.noreply.github.com>
2024-02-05 14:45:20 -0300

    feat: create html folder and some css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+      colors: {
+        'primary': '#BC01FD',
+        'primary-dark': '#252525',
+        'generic-dark-1': '#2A2537',
+        'generic-dark-2': '#241E34',
+        'generic-dark-3': '#35313E',
+      }
+    },
+    plugins: [],
+  }
