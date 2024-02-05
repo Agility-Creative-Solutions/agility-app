98d5c8e23de4088734e23611ed40817968602851
Felipe Macedo Gomes <104227174+femacedogomes@users.noreply.github.com>
2024-02-05 15:14:57 -0300

    fix: tailwind and directories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-      colors: {
-        'primary': '#BC01FD',
-        'primary-dark': '#252525',
-        'generic-dark-1': '#2A2537',
-        'generic-dark-2': '#241E34',
-        'generic-dark-3': '#35313E',
-      }
-    },
-    plugins: [],
-  }
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      'primary': '#BC01FD',
+      'primary-dark': '#252525',
+      'generic-dark-1': '#2A2537',
+      'generic-dark-2': '#241E34',
+      'generic-dark-3': '#35313E',
+    }
+  },
+  plugins: [],
+}
+
