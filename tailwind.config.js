8f5cf913908fb6829060ba9dcd6a9a5060300e1d
Felipe Macedo Gomes <104227174+femacedogomes@users.noreply.github.com>
2024-02-06 18:31:57 -0300

    css almost done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,12 @@ module.exports = {
     extend: {},
     colors: {
       'primary': '#A95FE3',
+      'secondary-purple-dark': '#2B2539',
       'primary-dark': '#252525',
       'generic-dark-1': '#2A2537',
       'generic-dark-2': '#241E34',
       'generic-dark-3': '#35313E',
+      'generic-purple-dark': '#262132',
       'generic-gray' : '#322E3C',
       'generic-gray-lighter' : "#CBCBCB",
       'white': "#FFFFFF"
