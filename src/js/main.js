4a1b0a8d780dc47567eda515f54a51efcbccfb9f
Felipe Macedo Gomes <104227174+femacedogomes@users.noreply.github.com>
2024-02-08 13:53:27 -0300

    improv responsive

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,14 @@ function goToWhatsapp () {
     const urlPath = `https://wa.me/5585996284730?text=${text}`;
     window.open(urlPath, '_blank').focus();
 }
+
+var menuButton = document.getElementById("menu-button");
+var menu = document.getElementById("menu-burguer");
+
+menuButton.addEventListener("click", function() {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+});
