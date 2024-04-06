2b185b7c09845023e26a01be48642c5dc9712770
Felipe Macedo Gomes <104227174+femacedogomes@users.noreply.github.com>
2024-04-06 16:41:11 -0300

    feat: first improvs to new version

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,11 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
-      'primary': '#A95FE3',
+      'primary': '#BC01FD',
+      'primary-lighter': "A95FE3",
       'secondary-purple-dark': '#2B2539',
-      'primary-dark': '#252525',
-      'generic-dark-1': '#2A2537',
-      'generic-dark-2': '#241E34',
-      'generic-dark-3': '#35313E',
+      'generic-dark-1': '#313131',
+      'generic-dark-2': '#242424',
       'generic-purple-dark': '#262132',
       'generic-gray' : '#322E3C',
       'generic-gray-lighter' : "#CBCBCB",
