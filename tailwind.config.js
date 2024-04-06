3b8a2387d6fb00f06b23db0b934f770407d3ad84
Felipe Macedo Gomes <104227174+femacedogomes@users.noreply.github.com>
2024-04-06 18:40:17 -0300

    feat: improv colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
       'primary-bg-dark-lighter': '#313131',
       'primary-bg-dark': '#242424',
       'primary-bg-darkness': '#1C1C1C',
-      'generic-gray' : '#322E3C',
+      'generic-gray' : '#373737',
       'generic-gray-lighter' : '#CBCBCB',
       'white': '#FFFFFF',
     }
