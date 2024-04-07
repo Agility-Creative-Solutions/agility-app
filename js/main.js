1a3ea6a81075a622918711c54a909f14ef38b483
renanpontez <renanpontez@gmail.com>
2024-04-07 12:23:31 -0300

    feat: improvements and tab fix

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
-function goToWhatsapp () {
-    const breakLine = `%0A`;
+function goToWhatsapp() {
+  const breakLine = `%0A`;
 
-    const name = document.getElementById("contact-name").value;
-    const email = document.getElementById("contact-email").value;
-    const message = document.getElementById("contact-message").value;
+  const name = document.getElementById("contact-name").value;
+  const email = document.getElementById("contact-email").value;
+  const message = document.getElementById("contact-message").value;
 
-    const text = `Me chamo ${name}, dono do email *${email}*.${breakLine} ${message}`;
+  const text = `Olá, Agility! Meu nome é ${name} (*${email}*).${breakLine}${breakLine}Mensagem: ${message}`;
 
-    const urlPath = `https://wa.me/5585996284730?text=${text}`;
-    window.open(urlPath, '_blank').focus();
+  const urlPath = `https://wa.me/5585996284730?text=${text}`;
+  window.open(urlPath, '_blank').focus();
 }
 
 var menuButton = document.getElementById("menu-button");
 var menu = document.getElementById("menu-burguer");
 
-menuButton.addEventListener("click", function() {
+menuButton.addEventListener("click", function () {
   if (menu.style.display === "block") {
     menu.style.display = "none";
   } else {
     menu.style.display = "block";
   }
+});
+
+var typed = new Typed('#randomTextElement', {
+  strings: ['criativa', 'rápida', 'inteligente', 'fácil'],
+  typeSpeed: 50,
+  backDelay: 3000,
+  loop: true,
 });
